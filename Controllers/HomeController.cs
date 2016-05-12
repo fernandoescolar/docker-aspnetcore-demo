8501f26d7e66cca7a97bef4fd1d9e7890939976c
fernandoescolar <fer.escolar@gmail.com>
2016-05-12 19:54:30 +0200

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿namespace docker_aspnetcore_demo.Controllers
+{
+    using Microsoft.AspNet.Mvc;
+    using Models;
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(User());
+        }
+
+        public User User()
+        {
+            User user = new User()
+            {
+                Name = "My name",
+                Address = "My address"
+            };
+
+            return user;
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,15 @@
+﻿namespace docker_aspnetcore_demo.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class User
+    {
+        [Required]
+        [MinLength(4)]
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public int Age { get; set; }
+    }
+}
